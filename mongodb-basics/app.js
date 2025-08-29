484a4560f07f093cf9b5bba614806985d8c410b5
Everest Paudel <paudeleverest09@gmail.com>
2025-08-29 16:07:53 +0545

    fix: correct user creation details and improve query logging in runQueryExample

--- mongodb-basics/app.js
+++ mongodb-basics/app.js
@@ -37,20 +37,27 @@ connectDB()
 
  async function runQueryExample() {
     try {
-        //Creaste a new Document
+        // Creaste a new Document
         const newUser = await User.create({
              name:"Everest paudel",
     email:"everest@gmail.com",
-    age: 20,
-    isActive:true,
+    age: 24,
+    isActive:false,
     tags:['developer','designer','manager'],
         })
         console.log("New user Created ",newUser)
 
+    //To get all the user whoese isActive is false
+ const getUserOfActiveFalse = await User.find({isActive :false})
+console.log("Data is Active false",getUserOfActiveFalse)
 
+const getLastCreatedUserByUserId = User.findById(newUser._id);
+console.log("Last Create User",getLastCreatedUserByUser)
+const selectedField = await User.find().select("name email -_id")
+console.log(selectedField)
 
-
-
+const limitedUser = await User.find().select("name email -_id").limit(5).skip(1);
+console.log(limitedUser)
         
     } catch (error) {
         console.log('Error->',error)
