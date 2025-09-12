7ab0348b693ee439a735c9c5e78aa69112c61389
Everest Paudel <paudeleverest09@gmail.com>
2025-09-12 18:22:28 +0545

    Feat :Change Password

--- 04_nodejs_Auth/controllers/auth-controller.js
+++ 04_nodejs_Auth/controllers/auth-controller.js
@@ -97,7 +97,59 @@ const loginUser = async (req, res) => {
     }
 };
 
+
+
+const changePassword = async(req ,res)=>{
+    try {
+        const userId = req.userInfo.userId
+        // extract old and new Password
+        const {oldPassword ,newPassword}= req.body
+
+        //find the 
+        const user = await  User.findById(userId)
+        if(!user){
+            return res.status(400).json({
+               success:false,
+               message:"User not found"
+            })
+        }
+        //check if the old password is correct
+        const isPasswordMatch = await bcrypt.compare(oldPassword ,user.password)
+
+        if(!isPasswordMatch){
+            res.status(400).json({
+                success:true,
+                message:"Old password did not match"
+            })
+        }
+
+        //hash the new password
+        const salt = await bcrypt.genSalt(10);
+        const newHashedPassword = await bcrypt.hash(newPassword ,salt)
+        
+
+        // update  user password and save to database
+   user.password = newHashedPassword;
+    await user.save()
+      
+        res.status(200).json({
+            success:true,
+            message:"Password Change succesfully"
+        })
+
+
+
+    } catch (error) {
+        console.error(error);
+         res.status(500).json({
+            success: false,
+            message: 'Something went wrong! Please try again.',
+        });
+    }
+}
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    changePassword
 };
--- 04_nodejs_Auth/routes/auth-routes.js
+++ 04_nodejs_Auth/routes/auth-routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/auth-controller');
+const { registerUser, loginUser, changePassword } = require('../controllers/auth-controller');
+const authMiddleware = require('../middleware/auth-middleware');
 const authRouter = express.Router();
 
 //all routes all related to user Authentication and Authorization
 
 authRouter.post('/register',registerUser);
 authRouter.post('/login',loginUser)
-
+authRouter.post('/change-password',authMiddleware,changePassword)
 
 
 
