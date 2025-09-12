0f2277e1711d4528dd71d77d6709fd6bbadc830c
Everest Paudel <paudeleverest09@gmail.com>
2025-09-12 10:02:24 +0545

    Feat: Admin Route Protect

--- 04_nodejs_Auth/routes/home-routes.js
+++ 04_nodejs_Auth/routes/home-routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authMiddleware = require('../middleware/auth-middleware')
+const isAdminUser = require('../middleware/admin-middleware')
 
 
 const router = express.Router()
