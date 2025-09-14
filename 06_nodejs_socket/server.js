808fc7a244ed6882557387350edbbd44f37f6d51
Everest Paudel <paudeleverest09@gmail.com>
2025-09-14 12:39:51 +0545

    Feat: Add user join functionality and update user list in chat application

--- 06_nodejs_socket/server.js
+++ 06_nodejs_socket/server.js
@@ -11,3 +11,31 @@ const server =http.createServer(app)
 const io = ws(server)
 
 app.use(express.static('public')) 
+
+const users = new Set()
+
+io.on('connection',(socket)=>{
+ console.log('A user is now connected')
+//handle users when  they will join
+socket.on('join',(userName)=>{
+users.add(userName)
+
+//broadcast to all client/users that a user has joined
+io.emit('userJoined',userName);
+
+//send the updated user list to all client
+io.emit('userList',Array.from(users))
+})
+
+
+//handle incoming chat message
+
+//handle user disconnection
+
+
+})
+
+
+app.listen(3001,()=>{
+    console.log("Server is running at port 3000")
+})
