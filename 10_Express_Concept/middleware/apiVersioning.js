ee64dabfeee52561da28a215feb5ae24761ca71e
Everest Paudel <paudeleverest09@gmail.com>
2025-09-15 13:00:48 +0545

    APIVersioning

---
+++ 10_Express_Concept/middleware/apiVersioning.js
@@ -0,0 +1,68 @@
+// const { version } = require("react");
+
+const { version } = require("react");
+
+const urlVersioning = (version) => (req, res, next) => {
+  if (req.path.startsWith(`/api/${version}`)) {
+    next();
+  } else {
+    res.status(404).json({
+      success: false,
+      error: "API version is not supported",
+    });
+  }
+};
+
+/*
+we can also write in this format simple urlversion take the argument and return the fucntion and function so return is express middleware
+const urlVersioning = (version) => {
+
+--------------------- 
+the return function is express middleware 
+because we need weather to continue the request by
+ calling next() or  dimiss it by giving response
+----------------------
+
+  return (req, res, next) => {
+    if (req.path.startsWith(`/api/${version}`)) {
+      next(); // ✅ Valid → continue
+    } else {
+      res.status(404).json({
+        success: false,
+        error: "API version is not supported"
+      }); // ❌ Invalid → stop here
+    }
+  };
+};
+
+
+*/
+
+const headerVersioning = (version) => (req, res, next) => {
+  if (req.get("Accept-Version") === version) {
+    next();
+  } else {
+    res.status(404).json({
+      success: false,
+      error: "API version is not supported",
+    });
+  }
+};
+
+const contentTypeVersioning = (version) => () => {
+  const contentType = req.get("Content-Type");
+  if (
+    contentType &&
+    contentType.includes(`application/vnd.api.${version} + json`)
+  ) {
+    next();
+  } else {
+    res.status(404).json({
+      success: false,
+      error: "API version is not supported",
+    });
+  }
+};
+
+
+module.exports ={urlVersioning ,headerVersioning ,contentTypeVersioning}
--- 10_Express_Concept/server.js
+++ 10_Express_Concept/server.js
@@ -1,9 +1,17 @@
+
+
+/*
+-> major release ->API ->v1 ,v2
+
+*/
+
 require('dotenv').config()
 const expess = require('express')
 const cors = require('cors')
 const configureCors = require('./config/corsConfig')
 const { requestLogger, addTimeStamp } = require('./middleware/customMiddleware')
 const { globalErrorhandler } = require('./middleware/errorHandler')
+const { urlVersioning } = require('./middleware/apiVersioning')
 const app = expess()
 const PORT =3000
 
@@ -11,6 +19,8 @@ const PORT =3000
 app.use(requestLogger)
 app.use(addTimeStamp)
 app.use(configureCors)
+
+app.use('/api/v1',urlVersioning('v1'))
 // express json middleware
 app.use(expess.json())
 
