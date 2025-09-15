59c7b53aad9c9ffe652ca98cb2d863bdeab5b3cc
Everest Paudel <paudeleverest09@gmail.com>
2025-09-15 12:00:57 +0545

    Global error and asyncHandler

--- 10_Express_Concept/middleware/errorHandler.js
+++ 10_Express_Concept/middleware/errorHandler.js
@@ -8,3 +8,35 @@ class APIError extends Error {
         this.name = 'APIError'
     }
 }
+
+const asyncHandler = (fn)=>(req ,res, next)=>{
+Promise.resolve(fn(req,res,next)).catch(next)
+}
+9
+//global error handler
+const globalErrorhandler = (err ,req,res,next)=>{
+    console.log(err.stack)  //log the error stack
+
+    if(err instanceof APIError){
+        return res.status(err.statusCode).json({
+            status :'Error',
+            message:err.message
+        })
+    }
+
+    // handle mongoose validation
+    else if(err.name ==='validationError'){
+          return res.status(400).json({
+            status :'Error',
+            message:"Validation error"
+        })
+    }
+    else {
+          return res.status(500).json({
+            status :'error',
+            message:"An unexpected error occur"
+        })
+    }
+}
+
+module.exports = {APIError ,asyncHandler ,globalErrorhandler }
--- 10_Express_Concept/server.js
+++ 10_Express_Concept/server.js
@@ -3,6 +3,7 @@ const expess = require('express')
 const cors = require('cors')
 const configureCors = require('./config/corsConfig')
 const { requestLogger, addTimeStamp } = require('./middleware/customMiddleware')
+const { globalErrorhandler } = require('./middleware/errorHandler')
 const app = expess()
 const PORT =3000
 
@@ -13,6 +14,8 @@ app.use(configureCors)
 // express json middleware
 app.use(expess.json())
 
+
+app.use(globalErrorhandler)
 app.listen(PORT, ()=>{
     console.log("Server is listening at Port",PORT)
 })
